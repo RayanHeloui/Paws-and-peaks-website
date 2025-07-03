693d788a5119d6e851c1be7a970ed329056c79c2
PaP <woof@pawsandpeaks.com.au>
2025-07-03 14:10:00 +1000

    Update send-welcome.js

--- api/send-welcome.js
+++ api/send-welcome.js
@@ -15,7 +15,7 @@ export default async function handler(req, res) {
 
   try {
     const data = await resend.emails.send({
-      from: 'onboarding@resend.dev',
+      from: 'woof@pawsandpeaks.com.au',
       to: email,
       subject: '🎉 Welcome to the Pack!',
       html: `
